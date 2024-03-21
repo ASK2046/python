e2293aa9f6de9151df97292f92a0e7e7c07b14ac
ASK2046 <164341326+ASK2046@users.noreply.github.com>
2024-03-21 16:18:57 +0500

    Add files via upload

---
+++ assignment no 1.py
@@ -0,0 +1,10 @@
+#program to check if a number is positive, negation or zero:
+
+num = 0
+
+if num > 0:
+    print ('it is a positive number')
+elif num == 0:
+    print ('it is zero')
+else:
+    print('it is a negation number')
